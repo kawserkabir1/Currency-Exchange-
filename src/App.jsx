4a6dedf3175602a924433ab6dda7de9835008553
Kawser Kabir <kawserkabir305@gmail.com>
2024-09-28 08:39:04 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ const App = () => {
   return (
     <div className="currency-converter">
       <h2 className="converter-title">Currency Converter</h2>
-      <p style="text-align: center; color: #fff;">By Cyber Tunnel</p>
       <ConverterForm />
     </div>
   )
